327aaed286bd4a3d8f6199b4d7e39e807a8e4865
bilcadina <adina15felicia@yahoo.com>
2020-12-07 21:05:48 +0200

    const & let

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
-var activePage = "skills";
+let activePage = "skills";
 
 function hide(id) {
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     if (el) {
         el.style.display = "none";
     } else {
@@ -11,26 +11,24 @@ function hide(id) {
 
 function hidePreviousPage () {
     hide(activePage);
-    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`);
     link.classList.remove("active");
 }
 
 function showPage(pageId) {
    hidePreviousPage();
     document.getElementById(pageId).style.display = "";
-    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`);
     link.classList.add("active");
     activePage = pageId;
 }
 
 function initMenu(){
     document.addEventListener("click", function(e){
-            var link = e.target;
+            const link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.innerHTML.toLowerCase();
-            console.info(id);
-            showPage(id);
-            link.classList.add("active");
+            const id = link.getAttribute("data-page");
+            showPage(id);   
         }
     })
 }
@@ -40,20 +38,15 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    var skillsLi = skills.map(function(skill){
+    return skillsLi = skills.map(function(skill){
             return `<li class=" ${skill.endorsments > 9 ? "favorite" : ""}">
             ${skill.name} <span>&middot; ${skill.endorsments}</span>
             </li>`;
-
-});
-return skillsLi.join("");
+}).join("");
 }
 
 function showSkills(skills) {
-    var html = getHTMLSkills(skills);
-
-
-    var ul = document.querySelector("#skills ul")
+    var ul = document.querySelector("#skills ul");
     ul.innerHTML = getHTMLSkills(skills);
     }
 
@@ -62,11 +55,9 @@ fetch("data/skills.json").then(function(r) {
 }).then(function(allSkills) {
     allSkills.sort(function(s1, s2) {
        return s2.endorsments - s1.endorsments;
-       //return s1.name > s2.name ? -1 : 1;
     });
-
 
     showSkills(allSkills);
 });
-//todo intro in es6
+
 
